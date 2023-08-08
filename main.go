127ba8a500f64780455e1acd765226a979a2d4da
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-08-08 10:18:20 -0400

    basic image resizing, via nfnt/resize

--- main.go
+++ main.go
@@ -1,31 +1,45 @@
 package main
- 
+
 import (
-    "fmt"
-    "image"
-    "image/png"
-    "log"
-    "os"
+	"image"
+	"image/png"
+	"log"
+	"os"
+
+	"github.com/nfnt/resize"
 )
- 
+
+const SIZE_MAX_WIDTH = 420
+
 func main() {
-    catFile, err := os.Open("/home/arkaprabham/Documents/Journal_Dev/Golang/github.com/image-op/cat.png")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer catFile.Close()
- 
-    imData, imType, err := image.Decode(catFile)
-    if err != nil {
-        fmt.Println(err)
-    }
- 
-    fmt.Println(imData)
-    fmt.Println(imType)
- 
-    cat, err := png.Decode(catFile)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(cat)
+	catFile, err := os.Open("original.png")
+	if err != nil {
+		log.Fatalf("failed open file: %s", err)
+	}
+	defer catFile.Close()
+
+	img, _, err := image.Decode(catFile)
+	if err != nil {
+		log.Fatalf("failed decode image: %s", err)
+	}
+
+	// resize image
+	// Calculate new dimensions.
+	newWidth := int(SIZE_MAX_WIDTH)
+	newHeight := (int(SIZE_MAX_WIDTH) * img.Bounds().Dy()) / img.Bounds().Dx()
+
+	// Resize the image.
+	m := resize.Resize(uint(newWidth), uint(newHeight), img, resize.Lanczos3)
+
+	// Save the resized image.
+	out, err := os.Create("resized.png")
+	if err != nil {
+		log.Fatalf("failed create file: %s", err)
+	}
+	defer out.Close()
+
+	err = png.Encode(out, m)
+	if err != nil {
+		log.Fatalf("failed encode image: %s", err)
+	}
 }
